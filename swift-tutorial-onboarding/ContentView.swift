a9d2a55df93138507d892a064e5c099c793874a2
R1013-T <takahashi.ryunosuke.job@gmail.com>
2024-12-30 18:00:59 +0900

    Initial Commit

---
+++ swift-tutorial-onboarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-tutorial-onboarding
+//
+//  Created by 髙橋龍之介 on 2024/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-tutorial-onboarding/swift_tutorial_onboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorial_onboardingApp.swift
+//  swift-tutorial-onboarding
+//
+//  Created by 髙橋龍之介 on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorial_onboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
